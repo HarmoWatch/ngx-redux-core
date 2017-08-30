eaed3e96c9cbd875af4368352cb3d780549c8f80
Kay Schecker <sayhello@kay-schecker.de>
2017-08-30 21:31:47 +0200

    covered reducer by unit test

---
+++ src/decorator/reducer.ts
@@ -0,0 +1,41 @@
+import { ReduxRegistry } from '../registry';
+import { ReduxActionFunc } from './action';
+
+export interface IReduxReducerAction<P = any> {
+  payload: P;
+}
+
+export type ReduxReducerFunc<S, P> = (state: S, action?: IReduxReducerAction<P>) => S;
+export type ReduxReducerDecorator<S, P>
+  = (target: object,
+     propertyKey: string | symbol,
+     descriptor: TypedPropertyDescriptor<ReduxReducerFunc<S, P>>) => TypedPropertyDescriptor<ReduxReducerFunc<S, P>>;
+
+export type ReduxReducerActionType<P> = ReduxActionFunc<Promise<P>> | ReduxActionFunc<P> | string;
+export type ReduxReducerActionTypeArray<P> = ReduxReducerActionType<P> | Array<ReduxReducerActionType<P>>;
+
+export function ReduxReducer<S = any, P = any>(type: ReduxReducerActionTypeArray<P>): ReduxReducerDecorator<S, P> {
+
+  const types = (Array.isArray(type) ? type : [ type ]).map((t) => {
+    if (typeof t === 'function') {
+
+      if (!t[ '__@ReduxAction' ] || !t[ '__@ReduxAction' ].type) {
+        throw new Error('Unable to resolve action type. Make sure you\'ve decorated the target by @ReduxAction');
+      }
+
+      return t[ '__@ReduxAction' ].type;
+    }
+
+    return t;
+  });
+
+  return (target: object, propertyKey: string) => {
+    if (typeof target === 'object') {
+      target[ propertyKey ].bind(target);
+    }
+
+    types.forEach(t => ReduxRegistry.registerReducer(t, target[ propertyKey ]));
+    return target[ propertyKey ];
+  };
+
+}
