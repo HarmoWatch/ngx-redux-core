39c47a9f29d41a6389657a482a45aebc41618d2c
Kay Schecker <sayhello@kay-schecker.de>
2017-10-05 00:09:01 +0200

    use provider for config

--- src/redux.module.ts
+++ src/redux.module.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { Inject, InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
 import { createStore, Store } from 'redux';
 import { ReduxSelectPipe } from './pipe/select/select';
 import { ReduxRegistry } from './registry';
 import { rootReducer } from './root-reducer';
+
+export const REDUX_MODULE_CONFIG = new InjectionToken<IReduxConfig>('REDUX_MODULE_CONFIG');
+export const IS_ROOT_MODULE = new InjectionToken<boolean>('IS_ROOT_MODULE');
 
 export interface IReduxConfig {
   store?: Store<{}>;
@@ -23,24 +26,37 @@ export interface IReduxConfig {
 })
 export class ReduxModule {
 
+  constructor(
+    @Inject(IS_ROOT_MODULE) isRootModule: boolean = false,
+    @Inject(REDUX_MODULE_CONFIG) config: IReduxConfig = null,
+  ) {
+
+    if (isRootModule) {
+      config = Object.assign({
+        store: null,
+      }, config || {});
+
+      ReduxRegistry.registerStore(config.store || createStore(rootReducer, {}));
+    }
+
+  }
+
   public static forChild(): ModuleWithProviders {
     return {
       ngModule: ReduxModule,
-      providers: [],
+      providers: [
+        {provide: IS_ROOT_MODULE, useValue: false},
+      ],
     };
   }
 
   public static forRoot(config?: IReduxConfig): ModuleWithProviders {
-
-    config = Object.assign({
-      store: null,
-    }, config || {});
-
-    ReduxRegistry.registerStore(config.store || createStore(rootReducer, {}));
-
     return {
       ngModule: ReduxModule,
-      providers: [],
+      providers: [
+        {provide: REDUX_MODULE_CONFIG, useValue: config},
+        {provide: IS_ROOT_MODULE, useValue: true},
+      ],
     };
   }
 
