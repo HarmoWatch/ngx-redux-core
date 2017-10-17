75bcf3652c3a2cbbd13c0a36b531c65576335654
Kay Schecker <sayhello@kay-schecker.de>
2017-10-17 20:45:01 +0200

    refactored redux action controller decorator

--- src/decorator/action/controller.ts
+++ src/decorator/action/controller.ts
@@ -1,11 +1,8 @@
+import { Type } from '@angular/core';
 import { MetadataManager } from '../../metadata/manager';
 
-export interface IReduxActionControllerType {
-  new (...args: any[]): any;
-}
-
 export function ReduxActionController(prefix: string) {
-  return <T extends IReduxActionControllerType>(constructor: T) => {
+  return <T extends Type<{}>>(constructor: T) => {
     MetadataManager.setActionContextMetadata(constructor, {prefix});
     return constructor;
   };
