a9b2f7a5ad0adafbfbd41b9d12fc7c03f782060c
Kay Schecker <sayhello@kay-schecker.de>
2017-12-14 12:02:16 +0100

    fix working on reference issue that results in an invalid decorated class

--- src/ngx-redux/metadata/manager.ts
+++ src/ngx-redux/metadata/manager.ts
@@ -12,32 +12,19 @@ export interface StateData {
 export class MetadataManager {
 
   private static readonly ACTION_KEY = Symbol('@ReduxAction');
-  private static readonly ACTION_DEFAULT: ReduxActionDecoratorMetadata = {
-    contextClazz: null,
-    type: '',
-  };
-
   private static readonly ACTION_CONTEXT_KEY = Symbol('@ReduxActionContext');
-  private static readonly ACTION_CONTEXT_DEFAULT: ActionContextData = {
-    prefix: '',
-  };
-
   private static readonly REDUCER_KEY = Symbol('@ReduxReducer');
-  private static readonly REDUCER_DEFAULT: ReduxReducerDecoratorMetadata = {
-    reducers: [],
-  };
-
   private static readonly STATE_KEY = Symbol('@ReduxState');
-  private static readonly STATE_DEFAULT: StateData = {
-    name: '',
-  };
 
   public static setActionMetadata<T>(target: T, data: ReduxActionDecoratorMetadata) {
     MetadataManager.set(target, MetadataManager.ACTION_KEY, data);
   }
 
   public static getActionMetadata<T>(target: T): ReduxActionDecoratorMetadata {
-    return MetadataManager.get(target, MetadataManager.ACTION_KEY, MetadataManager.ACTION_DEFAULT);
+    return MetadataManager.get(target, MetadataManager.ACTION_KEY, {
+      contextClazz: null,
+      type: '',
+    });
   }
 
   public static setActionContextMetadata<T>(target: T, data: ActionContextData) {
@@ -45,7 +32,9 @@ export class MetadataManager {
   }
 
   public static getActionContextMetadata<T>(target: T): ActionContextData {
-    return MetadataManager.get(target, MetadataManager.ACTION_CONTEXT_KEY, MetadataManager.ACTION_CONTEXT_DEFAULT);
+    return MetadataManager.get(target, MetadataManager.ACTION_CONTEXT_KEY, {
+      prefix: '',
+    });
   }
 
   public static setReducerMetadata<T>(target: T, data: ReduxReducerDecoratorMetadata) {
@@ -53,7 +42,9 @@ export class MetadataManager {
   }
 
   public static getReducerMetadata<T>(target: T): ReduxReducerDecoratorMetadata {
-    return MetadataManager.get(target, MetadataManager.REDUCER_KEY, MetadataManager.REDUCER_DEFAULT);
+    return MetadataManager.get(target, MetadataManager.REDUCER_KEY, {
+      reducers: [],
+    });
   }
 
   public static setStateMetadata<T>(target: T, data: StateData) {
@@ -61,7 +52,9 @@ export class MetadataManager {
   }
 
   public static getStateMetadata<T>(target: T): StateData {
-    return MetadataManager.get(target, MetadataManager.STATE_KEY, MetadataManager.STATE_DEFAULT);
+    return MetadataManager.get(target, MetadataManager.STATE_KEY, {
+      name: '',
+    });
   }
 
   private static set <D, T>(target: T, key: string | symbol, data: D) {
