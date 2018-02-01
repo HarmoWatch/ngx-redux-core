ea2906c6d01e082ced2beb4d6b898abf7daf8442
Kay Schecker <sayhello@kay-schecker.de>
2018-02-01 15:03:08 +0100

    expose new method "getActionType"

--- src/@harmowatch/ngx-redux-core/action/public_api.ts
+++ src/@harmowatch/ngx-redux-core/action/public_api.ts
@@ -1,6 +1,7 @@
-import { ReduxActionContextDecorator, ReduxActionDecorator } from '@harmowatch/redux-decorators';
+import { ReduxActionContextDecorator, ReduxActionDecorator, ReduxActionDispatcher } from '@harmowatch/redux-decorators';
 
 export const ReduxAction = ReduxActionDecorator.forMethod;
 export const ReduxActionContext = ReduxActionContextDecorator.forClass;
+export const getActionType = ReduxActionDispatcher.getType;
 
 export * from './interface';
