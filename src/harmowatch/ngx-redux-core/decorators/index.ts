ab01c7c32fa4643230f08d4a28e6d06d10b91a9b
Kay Schecker <sayhello@kay-schecker.de>
2018-02-08 23:27:13 +0100

    try to export alias function to avoid angulars compiler issue "Only initialized variables and constants can be referenced in decorators ..."

--- src/harmowatch/ngx-redux-core/decorators/index.ts
+++ src/harmowatch/ngx-redux-core/decorators/index.ts
@@ -1,13 +1,29 @@
 import {
   ReduxActionContextDecorator,
+  ReduxActionContextDecoratorConfig,
   ReduxActionDecorator,
+  ReduxActionDecoratorConfig,
   ReduxReducerDecorator,
+  ReduxReducerDecoratorConfig,
   ReduxStateDecorator,
+  ReduxStateDecoratorConfig,
 } from '@harmowatch/redux-decorators';
+import { MethodType } from '@harmowatch/redux-decorators/lib/generic/generic-decorator';
 
 export * from './redux-select.decorator';
 
-export const ReduxAction = ReduxActionDecorator.forMethod;
-export const ReduxActionContext = ReduxActionContextDecorator.forClass;
-export const ReduxState = ReduxStateDecorator.forClass;
-export const ReduxReducer = ReduxReducerDecorator.forMethod;
+export function ReduxAction(config?: ReduxActionDecoratorConfig): MethodType<Function> {
+  return ReduxActionDecorator.forMethod(config);
+}
+
+export function ReduxActionContext(config?: ReduxActionContextDecoratorConfig) {
+  return ReduxActionContextDecorator.forClass(config);
+}
+
+export function ReduxState(config?: ReduxStateDecoratorConfig) {
+  return ReduxStateDecorator.forClass(config);
+}
+
+export function ReduxReducer(config?: ReduxReducerDecoratorConfig) {
+  return ReduxReducerDecorator.forMethod(config);
+}
