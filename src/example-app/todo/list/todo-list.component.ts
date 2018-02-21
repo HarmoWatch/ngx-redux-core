ef06402fd1c5886eb694dbd84990dabe7fe9df0d
Kay Schecker <sayhello@kay-schecker.de>
2018-02-21 16:39:36 +0100

    use ReduxSelector instead of Observable

--- src/example-app/todo/list/todo-list.component.ts
+++ src/example-app/todo/list/todo-list.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
-import { Observable } from 'rxjs/Observable';
 import { v4 } from 'uuid';
 import { TodoModuleStateProvider } from '../todo.module.state.provider';
 import { TodoListItem } from './todo-list-item';
 import { ReduxAction, ReduxActionContext } from '../../../harmowatch/ngx-redux-core/decorators/index';
 import { ReduxSelect } from '../../../harmowatch/ngx-redux-core/decorators/redux-select.decorator';
+import { ReduxSelector } from '../../../harmowatch/ngx-redux-core/redux-selector';
 
 @Component({
   templateUrl: './todo-list.component.html',
@@ -14,7 +14,7 @@ import { ReduxSelect } from '../../../harmowatch/ngx-redux-core/decorators/redux
 export class TodoListComponent {
 
   @ReduxSelect('', TodoModuleStateProvider)
-  public state: Observable<{}>;
+  public state: ReduxSelector<{}>;
 
   @ReduxAction()
   add(label: string): TodoListItem {
