fe21290d3a51c9d66443f8733a1602243d1b47f2
Kay Schecker <sayhello@kay-schecker.de>
2018-03-29 12:51:25 +0200

    Fixed broken build

--- karma.conf.js
+++ karma.conf.js
@@ -1,18 +1,15 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-const path = require('path');
-
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular/cli', '@harmowatch/ngx-redux-core'],
+    frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma'),
-      require(path.resolve('dist/plugins/karma')),
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
